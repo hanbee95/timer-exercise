1da5a19543a2b6d6a110f71958014d4cbe448cb8
hanbee95 <nospicynocilantro@gmail.com>
2021-08-05 20:25:11 -0500

    first contents

---
+++ mysite/exercise/urls.py
@@ -0,0 +1,20 @@
+from django.urls import include, path
+
+from . import views
+#from .views import geeks_view
+app_name = 'exercise'
+urlpatterns = [
+    # ex: /exercise/
+    path('', views.index, name='index'),
+    path('timer', views.timer, name='timer'),   
+    path('timer/showtimer', views.showtimer, name='showtimer'), 
+    # ex: /exercise/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /exercise/5/results/
+    #path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /exercise/5/vote/
+    #path('<int:question_id>/vote/', views.vote, name='vote'),
+    #path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),    
+]
